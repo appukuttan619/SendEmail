3db8d8904302d9d996b334b7f74a4312f86ba14e
appukuttan619 <147173733+appukuttan619@users.noreply.github.com>
2025-03-04 12:47:56 +0530

    Update MailConfig.java

--- src/main/java/com/example/demo/config/MailConfig.java
+++ src/main/java/com/example/demo/config/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("shijas002@gmail.com");
-        mailSender.setPassword("cbpe msli vuum rtkj");
+        mailSender.setUsername("shijas002sdfs@gmail.com");
+        mailSender.setPassword("cbpe msdfsdsdfsdfsdf");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
